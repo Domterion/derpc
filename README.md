9705256da7582d5b4521022995211dbfb546d12c
Dominic Harris <tastykake563@gmail.com>
2021-07-01 00:45:18 -0400

    first owo

---
+++ README.md
@@ -0,0 +1,25 @@
+## DeRPC
+
+A rotating Discord Rich Presence configured with TOML
+
+### Using
+
+To use you will need Rust and Git on your system.
+
+```
+# clone the repo
+$ git clone https://github.com/Domterion/derpc
+
+# cd into the cloned repo
+$ cd derpc
+
+# build derpc for release
+$ cargo build --release
+
+# run derpc
+$ ./target/release/derpc
+```
+
+### Configuration
+
+A guide to the config can be found in `config.example.toml`
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+mod config;
+mod constants;
+
+use std::{thread, time::Duration};
+
+use discord_rich_presence::{
+    activity::{Activity, Assets, Button},
+    new_client, DiscordIpc,
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("{:?}", constants::CONFIG.configuration);
+    let mut client = new_client(&constants::CONFIG.configuration.client_id)?;
+
+    client.connect()?;
+
+    loop {
+        for (i, (k, v)) in constants::CONFIG.statuses.iter().enumerate() {
+            println!(
+                "({:?}/{:?}) Currently on {:?}",
+                i + 1,
+                constants::CONFIG.statuses.len(),
+                k
+            );
+
+            let mut activity = Activity::new();
+
+            activity = activity.state(&v.state);
+            activity = activity.details(&v.details);
+
+            let mut buttons = vec![];
+            if let Some(btns) = &v.buttons {
+                for button in btns {
+                    buttons.push(Button::new(&button.label, &button.url));
+                }
+            }
+
+            let mut assets = Assets::new();
+
+            let mut large_image = String::new();
+            if let Some(img) = &v.large_image {
+                large_image = img.to_string();
+            }
+            assets = assets.large_image(&large_image);
+
+            let mut small_image = String::new();
+            if let Some(img) = &v.small_image {
+                small_image = img.to_string();
+            }
+            assets = assets.small_image(&small_image);
+
+            if !large_image.is_empty() {
+                activity = activity.assets(assets);
+            }
+
+            if !buttons.is_empty() {
+                activity = activity.buttons(buttons);
+            }
+
+            client.set_activity(activity)?;
+
+            let sleep = Duration::from_secs(constants::CONFIG.configuration.time_between);
+            thread::sleep(sleep);
+        }
+    }
+
+    client.close()?;
+
+    Ok(())
+}
